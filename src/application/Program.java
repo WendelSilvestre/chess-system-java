32e222ca6ea4169c9f587479e0320fbfef1369e6
WendelSilvestre <wendelsilvestre9@gmail.com>
2020-07-20 17:32:25 -0300

    Checkmategit add .git add .

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,12 +13,12 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-	
+		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true){
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -33,22 +33,22 @@ public class Program {
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece capturedPiece = chessMatch.performChessMove(source,  target);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
-				if (captured != null) {
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				}
-			catch(ChessException e) {
+			}
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-			catch(InputMismatchException e) {
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 		}
-		
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
-	
 }
